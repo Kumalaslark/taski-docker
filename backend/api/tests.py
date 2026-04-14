1eca2c8761d2778b8c0f58d30d4a3ca27133f703
Илья Абакунчик <ill19072008@gmail.com>
2026-04-14 21:45:18 +0300

    Fix docstring

---
+++ backend/api/tests.py
@@ -0,0 +1,32 @@
+"""Модуль для тестирования API проекта Taski."""
+from http import HTTPStatus
+from api import models
+from django.test import Client, TestCase
+
+
+class TaskiAPITestCase(TestCase):
+    """Класс тестов для проверки API задач."""
+
+    def setUp(self):
+        """Подготовка клиента для тестов."""
+        self.guest_client = Client()
+
+    def test_list_exists(self):
+        """
+        Проверка доступности списка задач.
+
+        Запрос должен возвращать HTTP-код 200.
+        """
+        response = self.guest_client.get('/api/tasks/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_task_creation(self):
+        """
+        Проверка создания задачи.
+
+        Создаем задачу и проверяем, что она появилась в БД.
+        """
+        data = {'title': 'Test', 'description': 'Test'}
+        response = self.guest_client.post('/api/tasks/', data=data)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertTrue(models.Task.objects.filter(title='Test').exists())
