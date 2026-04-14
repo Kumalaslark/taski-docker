02ec4e8d671e59478b593acdc282f1d4f1faf260
Илья Абакунчик <ill19072008@gmail.com>
2026-04-14 21:38:52 +0300

    Fix docstring formatting (D100, D205)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,16 +1,17 @@
+"""Настройки Django-проекта Taski."""
 from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
-
+SECRET_KEY = (
+    'django-insecure-j_89af+30&&4qm*8z9_('
+    '^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+)
 DEBUG = True
 
 ALLOWED_HOSTS = ['158.160.242.75', 'localhost', '127.0.0.1', 'backend']
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -79,19 +80,30 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
